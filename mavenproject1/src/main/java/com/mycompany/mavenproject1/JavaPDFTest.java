68928408293a52a6585bac2f8868ad5732f665bd
Bruno <Bruno@BRUNO-PC.home>
2018-06-26 23:38:03 -0300

    etapa 1000

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/JavaPDFTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.mavenproject1;
+
+import java.io.IOException;
+
+public class JavaPDFTest {
+
+    public static void main(String[] args) throws IOException {
+
+       PDFManager pdfManager = new PDFManager();       
+       pdfManager.setFilePath("./src/main/resources/13.pdf");
+       System.out.println(pdfManager.ToText());       
+    
+    }    
+}
